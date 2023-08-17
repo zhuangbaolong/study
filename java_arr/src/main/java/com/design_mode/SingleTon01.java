09a037c568b5d7bdcfd8de3f61df9c10a19b5122
zhuangbl-bitware <zhuangbaolong@bit-ware.cn>
2023-08-17 17:24:34 +0800

    单例设计模式

---
+++ java_arr/src/main/java/com/design_mode/SingleTon01.java
@@ -0,0 +1,40 @@
+package com.design_mode;
+
+//饿汉式的单例模式
+public class SingleTon01 {
+    public static void main(String[] args) {
+        GirlFriend instance = GirlFriend.getInstance();
+        System.out.println(instance);
+        GirlFriend instance2 = GirlFriend.getInstance();
+        System.out.println(instance2);
+        System.out.println(instance == instance2);
+    }
+}
+
+class GirlFriend {
+    private String name;
+
+    private static GirlFriend gf = new GirlFriend("小红红");
+
+    /*
+    单例模式-饿汉式
+    1. 将构造器私有化
+    2. 在类的内部直接创造对象（该对象是static）
+    3. 提供一个公共的static方法，返回gf
+     */
+    private GirlFriend(String name) {
+        System.out.println("构造器");
+        this.name = name;
+    }
+
+    public static GirlFriend getInstance() {
+        return gf;
+    }
+
+    @Override
+    public String toString() {
+        return "GirlFriend{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ java_arr/src/main/java/com/design_mode/SingleTon02.java
@@ -0,0 +1,29 @@
+package com.design_mode;
+
+//懒汉式的单例模式
+public class SingleTon02 {
+
+}
+class Cat{
+    private String name;
+    public static int n1 = 999;
+    private static Cat cat;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public static Cat getInstance(){
+        if (cat == null){
+            cat = new Cat("小猫");
+        }
+        return cat;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
